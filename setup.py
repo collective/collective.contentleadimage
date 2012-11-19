7a0ba2bbdc92e9014f04cd3f6a683a0faabb560c
Maurits van Rees <maurits@vanrees.org>
2012-11-19 17:41:02 +0100

    Renamed docs/HISTORY.txt to CHANGES.rst.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.3.4.dev0'
 
@@ -9,7 +8,7 @@ setup(name='collective.contentleadimage',
       version=version,
       description="Adds lead image to any content in plone site",
       long_description=(open("README.rst").read() + "\n\n" +
-                        open(os.path.join("docs", "HISTORY.txt")).read()),
+                        open("CHANGES.rst").read()),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           "Framework :: Plone",
